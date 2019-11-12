ae47daa6225efb2d63a310c68235202f817be9e5
Bill061 <september217@gmail.com>
2019-11-13 02:51:48 +0800

    d1

---
+++ src/M2Q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class M2Q1 {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        float max=Float.MAX_VALUE;
+        float min=-Float.MAX_VALUE;
+        for(int i=0;i<10;i++) {
+            float n=scn.nextFloat();
+            if (max < n) {
+                max = n;
+            }
+            if (min > n) {
+                min = n;
+            }
+        }
+        System.out.printf("max=%.2f\n", max);
+        System.out.printf("min=%.2f\n", min);
+
+    }
+}
